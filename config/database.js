99853c3fb08ea58ad84798d1e837baafaac53139
Tyler Blake <tyler.blake.louisville@gmail.com>
2018-01-03 20:36:34 -0500

    Fix depricating mongoose connect messages

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     uri: 'mongodb://user:test@ds161426.mlab.com:61426/meanauth',
-    options: { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
-                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } }
+    options: {                
+                useMongoClient: true
             },
     secret: 'yourSecret'
 }
