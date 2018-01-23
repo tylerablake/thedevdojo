cba6b210e1001c17609ad6cf58edfea90603486d
Tyler Blake <tyler.blake.louisville@gmail.com>
2018-01-22 20:07:10 -0500

    initial commit of styling changes, update dashboard and register page to use bulma.io

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -52,7 +52,7 @@ const appRoutes: Routes = [
   {path: 'posts/create', component: PostComponent},
   {path: 'posts/:id', component: ViewPostComponent},
   {path: 'posts/tag/:tag', component: PostListByTagComponent},
-  {path: 'posts/tag/:tag/true', component: PostListByTagComponent}
+  {path: 'posts/tag/:tag/true', component: PostListByTagComponent},
   {path: 'posts/tag/:tag/false', component: PostListByTagComponent}
 ];
 
