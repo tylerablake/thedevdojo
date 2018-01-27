c5b6a85e86880c4ae163a5a9f3b1587a2896208c
Tyler Blake <tyler.blake.louisville@gmail.com>
2018-01-26 21:07:50 -0500

    Cleaning pending changes

--- angular-src/src/app/components/category-list/category-list.component.ts
+++ angular-src/src/app/components/category-list/category-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-category-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit() {
   }
